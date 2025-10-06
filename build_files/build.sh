33414dc7a64945ad67fe8233edbe0e90d543afac
7yv34 <dominik.tieber@titan-sec.com>
2025-10-06 19:16:05 +0200

    added hyprland

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,33 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux bolt cifs-utils kio-fuse
+dnf5 install -y \
+    tmux \
+    bolt \
+    cifs-utils \
+    kio-fuse \
+    alacritty \
+    alsa-utils \
+    blueman \
+    brightnessctl \
+    grim \
+    jq \
+    kitty \
+    libnotify \
+    lxqt-policykit \
+    nautilus \
+    pamixer \
+    pavucontrol \
+    playerctl \
+    rofi \
+    slurp \
+    swayimg \
+    swappy \
+    swaynotificationcenter \
+    wofi \
+    waybar \
+    wl-clipboard \
+    wlogout
 
 # Use a COPR Example:
 #
@@ -18,6 +44,13 @@ dnf5 install -y tmux bolt cifs-utils kio-fuse
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+dnf5 copr enable solopasha/hyprland 
+dnf5 install -y \
+    hyprland \
+    hyprlock \
+    hyprshot \
+    swww
+dnf5 copr disable solopasha/hyprland 
 
 #### Example for enabling a System Unit File
 
